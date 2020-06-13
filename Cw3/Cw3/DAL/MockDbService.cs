5cc27f7b1846332ba38382e73b3d2006d6316ef0
Wpawleniak <53783284+Wpawleniak@users.noreply.github.com>
2020-06-13 13:25:38 +0200

    Utworzono MockDbService

---
+++ Cw3/Cw3/DAL/MockDbService.cs
@@ -0,0 +1,28 @@
+﻿using Cw3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw3.DAL
+{
+    public class MockDbService : IdbService
+    {
+        private static IEnumerable<Student> studentsCollection;  
+
+        static MockDbService()
+        {
+            studentsCollection = new List<Student>
+            {
+                new Student{ IdStudent =1, FirstName= "Jan", LastName ="Kowalski"},
+                new Student{ IdStudent =1, FirstName= "Marian", LastName ="Paździoch"},
+                new Student{ IdStudent =1, FirstName= "Ferdynand", LastName ="Kiepski"},
+            };
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return studentsCollection; 
+        }
+    }
+}
