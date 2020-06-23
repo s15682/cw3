2d4de88d364ba762f0cc2b4d512246d38dbef830
Wpawleniak <53783284+Wpawleniak@users.noreply.github.com>
2020-06-23 11:01:55 +0200

    Cw5/dodanie enrollments controler

---
+++ Cw3/Cw3/Controllers/EnrollmentsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents(string orderby)
+        {
+            return Ok("coś");
+        }
+
+        [HttpPost]
+        public IActionResult EnrollStudent()
+        {
+            return Ok(); 
+        }
+
+
+    }
+}
